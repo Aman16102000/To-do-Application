08532a03d2f3ce991a52f6a3188324839e20b891
Aman Thakur <amanthakur5908@gmail.com>
2021-04-22 13:21:19 +0530

    Organized List spacing and margin

--- index.js
+++ index.js
@@ -1,116 +1,140 @@
 var count=0;
-
-function add()
+var chosedLi="";
+function addTask()
 {
-    // cheking if the input is empty or not
-
-    if(document.getElementById("addInput").value=="")
-    {
-      // if input is empty it will show the model 
-      document.getElementById("modal_body").innerHTML="Write something to add...";
-
-     $('#exampleModal').modal('show');
-
-    }
-    else
-    {
-
-    var list=document.createElement("li");
-
-    list.setAttribute("id",count); // setting count for every li element it helps us at time to find out which element to delete
-    list.setAttribute("class","list-group-item list-group-item-primary");
-
-    // setting checkbox in every element in list
-    var completeCheckBox=document.createElement("input");
-    completeCheckBox.setAttribute("type","checkbox");
-    completeCheckBox.setAttribute("style","margin-left:20px;");
-
-    // fetched data from the client and make create a textNode with respect to it
-    var fetchedData=document.getElementById("addInput").value;
-    var spanNode=document.createElement("span");
-    var node=document.createTextNode(fetchedData);
-    spanNode.appendChild(node);
+  let input =document.getElementById("addInput").value;
+  // if input is empty
+  if(input=="")
+  {
+    document.getElementById("modal_body").innerHTML="Write something to add...";
+    $('#exampleModal').modal('show');
+  }
+  else
+  {
+    let li=document.createElement("li");
+    li.setAttribute("id",count);// unique id of every list element
+    li.setAttribute("class","list-group-item list-group-item-primary");
     
-    // creating remove button to delete selected list element
-     var del=document.createElement("BUTTON");
-     del.setAttribute("value",count);
-     del.setAttribute("onclick","rem(this)");
-     del.setAttribute("type","button");
-     del.setAttribute("class","close");
-     var sp=document.createElement("span");
-     sp.setAttribute("aria-hidden","true");
-     var spbtn=document.createTextNode("remove")
+    //column 2
+    var divMain=document.createElement("div");
+    var divRow=document.createElement("div");
+    var divCol=document.createElement("div");
+    var p1=document.createElement("p");
+    
+    p1.setAttribute("style","word-wrap:break-word;")
+    divCol.setAttribute("class","col-6 col-sm-6");
+    divRow.setAttribute("class","row");
+    let divColTextNode=document.createTextNode(input);
+    p1.appendChild(divColTextNode);
+    divCol.appendChild(p1);
+    // end of column 2 
 
- 
-     // edit button
-     var edit=document.createElement("BUTTON");
-     edit.setAttribute("value",count);
-     edit.setAttribute("type","button")
-     edit.setAttribute("class","btn")
-     edit.setAttribute("style","background-color:blue; margin-left:50px; color:white;")
-     edit.setAttribute("onclick","edit(this)");
-     var editNode=document.createTextNode("Edit");
-     edit.appendChild(editNode);
+    // column 1
+    var divCol2=document.createElement("div");
+    var checkbox=document.createElement("input");
+    var checkBoxId=count+"checkBox";
+    divCol2.setAttribute("class","col-2 col-sm-2")
+    checkbox.setAttribute("type","checkbox");
+    checkbox.setAttribute("id",checkBoxId);
+    checkbox.setAttribute("onclick","isChecked(this)");
+    checkbox.setAttribute("value",count);
+    divCol2.appendChild(checkbox);
+    //end of column
 
-     // append all elements with respect to each other
-     sp.appendChild(spbtn);
-     del.appendChild(sp);
-     list.appendChild(spanNode);
-     list.appendChild(completeCheckBox);
-     list.appendChild(edit);
-     list.appendChild(del);
+    // column 3
+    var divCol3=document.createElement("div");
+    var remove=document.createElement("button");
 
-     
+    divCol3.setAttribute("class","col-2 col-sm-2");
+    remove.setAttribute("type","button");
+    remove.setAttribute("class","btn");
+    remove.setAttribute("value",count);
+    remove.setAttribute("style","background-color:blue; color:white");
+    remove.setAttribute("onclick","deleteLi(this)");
+    var deleteLiTextNode=document.createTextNode("Delete Task");
+    remove.appendChild(deleteLiTextNode);
+    divCol3.appendChild(remove);
 
-     // code to view list on browser
-     var element=document.getElementById("list");
-     element.insertBefore(list,element.childNodes[0]);
+    // column 4
+    var divCol4=document.createElement("div");
+    var edit=document.createElement("button");
+    divCol4.setAttribute("class","col-2 col-sm-2");
+    edit.setAttribute("class","btn");
+    edit.setAttribute("style","background-color:blue; color:white;");
+    edit.setAttribute("value",count);
+    edit.setAttribute("onclick","edit(this)")
+    var editTextNode=document.createTextNode("Edit");
+    edit.appendChild(editTextNode);
+    divCol4.appendChild(edit);
 
-     // On every succesfull addition in the list the input text value become empty
-     document.getElementById("addInput").value="";
-     count++;
 
-     // abled update button after first add in the list
-     document.getElementById("update").removeAttribute("disabled");
-    }
+    divRow.appendChild(divCol2);
+    divRow.appendChild(divCol);
+    divRow.appendChild(divCol4);
+    divRow.appendChild(divCol3);
+    divMain.appendChild(divRow);
+    li.appendChild(divMain);
 
-}
 
-function rem(objBtn)
-{
-   // remove selected element in the list
-   console.log(objBtn);
-   document.getElementById(objBtn.value).remove();
-}
-var input="";
-function edit(objBtn)
-{
-  console.log(objBtn);
-  input=document.getElementById(objBtn.value);
-  console.log(input);
-  document.getElementById("addInput").value=input.firstElementChild.textContent;
+    let list=document.getElementById("list");
+    list.insertBefore(li,list.childNodes[0]);
 
-  // var newNode=document.getElementById("addInput").value;
-  // input.firstElementChild.textContent=newNode;
-//  console.log(list.childNodes[objBtn.value]);
+    // On every succesfull addition in the list the input text value become empty
+    document.getElementById("addInput").value="";
+    count++;
+   
+  }
+  chosedLi="";
 }
 
 function update()
 {
-  if(document.getElementById("addInput").value=="" || input=="")
+  var changedText=document.getElementById("addInput").value;
+  if(chosedLi=="")
   {
-    // if input is empty it will show the model 
-    document.getElementById("modal_body").innerHTML="First choose then Update";
+    document.getElementById("modal_body").innerHTML="First choose then update";
    $('#exampleModal').modal('show');
-
   }
-  else{
-    var newNode=document.getElementById("addInput").value;
-   input.firstElementChild.textContent=newNode;
-
-   document.getElementById("addInput").value="";
-
-   // reset the edit click
-   input="";
+  else if(changedText=="")
+  {
+    document.getElementById("modal_body").innerHTML="Write something to update..";
+   $('#exampleModal').modal('show');
+  }
+  else
+  {
+  chosedLi[1].firstChild.textContent=changedText;
+  document.getElementById("addInput").value="";
+  chosedLi="";
   }
 }
+function edit(obj)
+{
+  //console.log(obj);
+  var  chosedBtn=document.getElementById(obj.value);
+  chosedLi=chosedBtn.firstElementChild.firstElementChild.childNodes;
+  var textInP=chosedLi[1].firstChild.textContent;
+  document.getElementById("addInput").value=textInP;
+}
+
+function deleteLi(obj)
+{
+  document.getElementById(obj.value).remove();
+}
+
+function isChecked(val)
+{
+  let checkbox=document.getElementById(val.id);
+
+  if(checkbox.checked==true)
+  {
+    document.getElementById(val.value).style.backgroundColor="green";
+    document.getElementById(val.value).style.color="white";
+
+  }
+  else
+  {
+    document.getElementById(val.value).style.backgroundColor="white";
+    document.getElementById(val.value).style.color="black";
+  }
+}
+
